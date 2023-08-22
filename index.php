d84065de3b6d43db5e5073862dbea3e56ce2ec8f
Rinaldi2212 <106576900+Rinaldi2212@users.noreply.github.com>
2023-08-22 07:02:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portofolio</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="text-center mt-5">
+                    <h1>Rinaldi Fransius Simbolon</h1>
+                    <p>UPN Veteran Jawa Timur</p>
+                </div>
+                <hr>
+                <h2>Tentang Saya</h2>
+                    <p>Saya adalah mahasiswa studi independen batch 5 yang ikut ke dalam GITS.iD.</p>
+                    <img src="gambardiri.webp" alt="Foto Profil Anda" class="img-fluid">
+
+                <h2>Portofolio</h2>
+                <div class="card">
+                    <div class="card-body">
+                        <h3>Website Sex Education</h3>
+                        <p>Menampilkan apa saja yang perlu dipelajari dalam website sex educatian.</p>
+                    </div>
+                
+                <h2>Kontak</h2>
+                <p>Email: rinaldisimbolon9@gmail.com.</p>
+                <p>LinkedIn: <a href="https://www.linkedin.com/in/rinaldi-simbolon">LinkedIn Profil</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
